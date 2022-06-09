0d896c3c1c7668b42dff792f90e9d781555d2964
PdcShoun <pdc.pachara@gmail.com>
2022-06-09 20:35:21 +0700

    First commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam fuga illo tempore mollitia aut dicta accusantium rerum magnam eum debitis quis magni, repellendus aspernatur recusandae labore veniam, a facere quam?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam fuga illo tempore mollitia aut dicta accusantium rerum magnam eum debitis quis magni, repellendus aspernatur recusandae labore veniam, a facere quam?</p>
   </div>
 </template>
